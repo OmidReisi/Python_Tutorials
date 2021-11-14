98da8be45151509e827b9d860cf3b7b25f388f1d
OmidReisi <omid79.dg@gmail.com>
2021-11-14 12:26:15 +0330

    added numbers.py file

---
+++ Basics/Variables/numbers.py
@@ -0,0 +1,50 @@
+num_1 = 3
+num_2 = 3.14
+
+print(type(num_1))
+print(type(num_2))
+
+
+# Arithmetic Operators:
+# Addition:       3 + 2
+# Subtraction:    3 - 2
+# Multiplication: 3 * 2
+# Division:       3 / 2
+# Floor Division: 3 // 2
+# Exponent:       3 ** 2
+# Modulus:        3 % 2
+
+
+# Comparisons:
+# Equal:            3 == 2
+# Not Equal:        3 != 2
+# Greater Than:     3 > 2
+# Less Than:        3 < 2
+# Greater or Equal: 3 >= 2
+# Less or Equal:    3 <= 2
+
+
+num_1 += 1
+print(num_1)
+
+# abs returns the absolute value
+print(abs(-3))
+
+# returns the round of the number to the given point of decimal
+print(round(num_2, 1))
+
+print(num_2)
+
+print(num_1 == num_2)
+print(num_1 != num_2)
+
+num_1 = "100"
+num_2 = "200"
+
+print(num_1 + num_2)
+
+# casting strings to integers
+num_1 = int(num_1)
+num_2 = int(num_2)
+
+print(num_1 + num_2)
