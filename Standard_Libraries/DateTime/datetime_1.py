ab13907a9000160476442fe6fdec4ac0e8beeb08
OmidReisi <omid79.dg@gmail.com>
2021-11-29 22:22:55 +0330

    new files were added
           modified:   .gitignore
            modified:   Standard_Libraries/DateTime/datetime_1.py
            new file:   Standard_Libraries/DateTime/datetime_2.py
            new file:   Standard_Libraries/Pip/pip_1.txt

--- Standard_Libraries/DateTime/datetime_1.py
+++ Standard_Libraries/DateTime/datetime_1.py
@@ -1,4 +1,71 @@
 import datetime
 
-today = datetime.date.today()
-print(today)
+# naive dates and times don't have timezones and daylight savings times
+# aware dates and times do have timezones and daylight savings times.
+
+
+# date takes only year, month and day with normal integers (don't zero pad the arguments)
+d = datetime.date(2021, 11, 29)
+print(d)
+
+tday = datetime.date.today()
+print(tday)
+
+# date objects have year, month and day attributes
+print(tday.year)
+
+# both return the day of the week and monday is the first day but weekday starts at 0 and isoweekday starts at 1
+print(tday.weekday())
+print(tday.isoweekday())
+
+
+# timedelta is just the difference between two dates or times
+tdelta = datetime.timedelta(days=7)
+print(tdelta)
+
+print()
+print()
+
+print(tday + tdelta)
+
+
+# date2 = date1 +/- timedelta
+# timedelta = date2 - date1
+
+# adding two datetime objects results in an error
+
+bday = datetime.date(2022, 8, 30)
+
+# this returns a timedelta object
+till_bday = bday - tday
+
+print(till_bday.days)
+# returns the timedelta in seconds
+print(till_bday.total_seconds())
+
+print()
+print()
+
+# time takes hour(in 24 hour format), minute, seconds, microseconds and tzinfo
+t = datetime.time(15, 1, 20, 300000)
+print(t)
+print(t.hour)
+
+
+print()
+print()
+
+# datetime object contains both date and time
+dt = datetime.datetime(2021, 11, 29, 15, 4, 25, 300000)
+print(dt)
+print(dt.year)
+
+# returns time portion of datetime
+print(dt.time())
+
+# return date portion of datetime
+print(dt.date())
+
+tdelta = datetime.timedelta(hours=12)
+
+print(dt + tdelta)
